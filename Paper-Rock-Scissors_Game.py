3ae0d7ce311e65263208e25e28ffab224f371bee
huyiyiyiyiyiyi <thaihuy0903@gmail.com>
2022-07-11 23:54:43 +0700

    'rps'

---
+++ Paper-Rock-Scissors_Game.py
@@ -0,0 +1,27 @@
+import random
+
+choices = ['rock','paper','scissors']
+computer_choice = random.choice(choices)
+player_choice = input('rock/paper/scissors?')
+print(computer_choice)
+if player_choice == computer_choice:
+    print('draw')
+elif player_choice == 'scissors':
+    if computer_choice == 'rock':
+        print('you lose')
+    elif computer_choice == 'paper':
+        print ('you win')    
+elif player_choice == 'rock':
+    if computer_choice == 'paper':
+        print('you lose')
+    elif computer_choice == 'scissors':
+        print ('you win')      
+elif player_choice == 'paper':
+    if computer_choice == 'scissors':
+        print('you lose')
+    elif computer_choice == 'rock':
+        print ('you win')  
+else:
+    print('error input')        
+
+
